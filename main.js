6561bdd2668453a7ea386074cc54ae967094fa02
Ntwari Moise <mozayntwali@gmail.com>
2021-04-27 23:58:56 +0200

    Single use components

--- main.js
+++ main.js
@@ -155,27 +155,33 @@
 //     }
 // });
 
-Vue.component('modal', {
-    template: `
-    <div class="modal is-active">
-        <div class="modal-background"></div>
-            <div class="modal-card">
-            <header class="modal-card-head">
-                <p class="modal-card-title">
-                    <slot name="header"></slot>
-                </p>
-                <button class="delete" aria-label="close"></button>
-            </header>
-            <section class="modal-card-body">
-                <slot></slot>
-            </section>
-            <footer class="modal-card-foot">
-                <slot name="footer"></slot>
-            </footer>
-        </div>
-    </div>
-    `
-})
+// Vue.component('modal', {
+//     template: `
+//     <div class="modal is-active">
+//         <div class="modal-background"></div>
+//             <div class="modal-card">
+//             <header class="modal-card-head">
+//                 <p class="modal-card-title">
+//                     <slot name="header"></slot>
+//                 </p>
+//                 <button class="delete" aria-label="close"></button>
+//             </header>
+//             <section class="modal-card-body">
+//                 <slot></slot>
+//             </section>
+//             <footer class="modal-card-foot">
+//                 <slot name="footer"></slot>
+//             </footer>
+//         </div>
+//     </div>
+//     `
+// })
+
+Vue.component('progress-view', {
+    data() {
+        return { completionRate : 0 };
+    }
+});
 
 new Vue({
     el: '#admin'
